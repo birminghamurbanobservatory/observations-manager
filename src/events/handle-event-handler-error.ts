631f383dcb4ed2873380c1df6c4a6e7234fbc5e8
SiBell <SiBellUK@gmail.com>
2020-05-05 10:36:27 +0100

    update to error levels

--- src/events/handle-event-handler-error.ts
+++ src/events/handle-event-handler-error.ts
@@ -1,6 +1,7 @@
 import {OperationalError} from '../errors/OperationalError';
 import {UnexpectedError} from '../errors/UnexpectedError';
 import * as logger from 'node-logger';
+import {DatabaseError} from '../errors/DatabaseError';
 
 export function logCensorAndRethrow(eventName, err): any {
   
@@ -8,8 +9,13 @@ export function logCensorAndRethrow(eventName, err): any {
   // Operational Errors
   //------------------------
   if (err instanceof OperationalError) {
-    // For now at least, I'll log these as as a full error.
-    logger.error(`Operational error whilst handling ${eventName} event.`, err);
+    
+    if (err instanceof DatabaseError) {
+      logger.error(`Database error whilst handling ${eventName} event.`, err);
+    } else {
+      // For example if a user requests a resource with the wrong ID I only want a 'warn' error not a full 'error' level error. 
+      logger.warn(`Operational error whilst handling ${eventName} event.`, err);
+    }
     throw err;
 
   //------------------------
